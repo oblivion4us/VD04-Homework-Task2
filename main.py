71cc5637799043d154a00815b2519dd1975d7bd9
oblivion4us <oblivion4us@icloud.com>
2024-10-31 14:41:45 +0200

    update main.py and add new image

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ app = Flask(__name__)
 def main():
     return render_template("index.html")
 
-@app.route("/blog/")
-def person():
-    return render_template("blog.html")
+@app.route("/discography/")
+def discography():
+    return render_template("discography.html")
 
 @app.route("/contacts/")
-def person():
+def contacts():
     return render_template("contacts.html")
 
 if __name__ == "__main__":
